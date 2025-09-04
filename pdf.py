47b6f4d20395db0f87178b50216d0a1f55615e2a
MarcoA20M <mcarcamohernandez20@gmail.com>
2025-09-03 21:19:04 -0600

    Logica de gemini a ver si sirve modifcado del depsek 8

--- pdf.py
+++ pdf.py
@@ -11,8 +11,8 @@ from flask_cors import CORS
 # Gunicorn se encargará de usar la variable de entorno $PORT en Render.
 
 app = Flask(__name__)
-# Configuración más explícita de CORS para permitir todas las solicitudes
-CORS(app, resources={r"/*": {"origins": "*"}})
+# Configuración de CORS para permitir todas las solicitudes desde cualquier origen.
+CORS(app)
 
 def highlight_pdf_with_rondas_folios(
     pdf_path: str,
@@ -20,11 +20,12 @@ def highlight_pdf_with_rondas_folios(
     cargas: List[dict],
     num_casillas_extra: int = 14,
 ) -> str:
+    """
+    Procesa un PDF para resaltar términos y añadir anotaciones basadas en una lista de cargas.
+    """
     doc = fitz.open(pdf_path)
-
     temp_output_fd, temp_output_path = tempfile.mkstemp(suffix=".pdf")
     os.close(temp_output_fd)
-
 
     folio_to_info_map = {
         str(carga.get("folio", "")).strip().upper(): {
@@ -34,12 +35,8 @@ def highlight_pdf_with_rondas_folios(
         }
         for carga in cargas if carga.get("folio") is not None
     }
-
     folios_ids_from_cargas = list(folio_to_info_map.keys())
-
-    
     search_flags = fitz.TEXT_DEHYPHENATE | fitz.TEXT_PRESERVE_WHITESPACE
-
 
     X_OFFSET_ANNOTATIONS = 0
     Y_OFFSET_ABOVE_DEPT = -13
@@ -166,24 +163,6 @@ def highlight_pdf_with_rondas_folios(
 def procesar_pdf():
     """
     Función que maneja la solicitud POST para procesar un archivo PDF.
-
-    **NOTA IMPORTANTE SOBRE EL TIEMPO DE ESPERA (TIMEOUT):**
-    Los registros de error indican que el trabajador de Gunicorn se está agotando
-    el tiempo de espera (`WORKER TIMEOUT`), lo que hace que el proceso falle
-    y el servidor se reinicie. Esto ocurre porque el procesamiento de PDF para un
-    documento grande y una larga lista de términos de búsqueda tarda más de 30 segundos
-    (el tiempo de espera predeterminado de Gunicorn en Render).
-
-    Para solucionar esto, necesitas aumentar el tiempo de espera de Gunicorn en la
-    configuración de tu servicio en Render. En el panel de control de Render, ve a:
-    Settings -> Environment -> Add Environment Variable.
-
-    Añade las siguientes variables y valores:
-    - **Key:** `GUNICORN_CMD_ARGS`
-    - **Value:** `--timeout 120` (o un valor mayor, como 300, si es necesario)
-
-    Esto aumentará el tiempo de espera a 120 segundos, dándole al servidor más
-    tiempo para procesar la solicitud.
     """
     try:
         if "file" not in request.files:
@@ -197,59 +176,58 @@ def procesar_pdf():
             return jsonify({"error": "Formato de 'cargas' inválido."}), 400
 
         file = request.files["file"]
-
         input_fd, input_path = tempfile.mkstemp(suffix=".pdf")
         os.close(input_fd)
         file.save(input_path)
 
         search_terms = [
-        "AAE70", "AAM10", "AAM11", "AAM12", "AAN20", "AAN30", "AAN50",
-    "AAP10", "AAY10", "ABA10", "ABA20", "ABA30", "ABA31", "ABB20",
-    "ABL10", "ABV30", "ACA10", "ACC10", "ACC20", "ACT20", "ADC10",
-    "ADC30", "ADG10", "ADI10", "ADI20", "ADI30", "ADL10", "ADN10",
-    "ADN11", "ADN12", "ADN13", "ADN14", "ADN30", "ADO10", "ADP10",
-    "ADP20", "ADR10", "ADS10", "ADT20", "ADV10", "ADV30", "AED10",
-    "AEE10", "AFM10", "AFW10", "AHA10", "AHS20", "AHU30", "AHU40",
-    "AHU50", "AIC30", "AIM10", "AMP10", "ANR10", "ANV10", "APC10",
-    "APC20", "APC34", "APZ10", "ASC10", "ASF10", "ASO10", "AST10",
-    "ATD10", "ATI11", "ATI20", "ATI30", "ATT20", "ATT30", "ATT40",
-    "CAC10", "CAH10", "CAT10", "CCA20", "CCA40", "CCH10", "CCH11",
-    "CCH70", "CCM55", "CCM60", "CCP10", "CCP15", "CCU20", "CEE20",
-    "CEH10", "CFC10", "CFP10", "CML10", "CPE10", "CPF10", "CVG10",
-    "DSA10", "EAA10", "EAV10", "ECE20", "ECE22", "ECN20", "ENV10",
-    "ERA10", "ERA20", "ERA50", "ERA51", "ERA53", "ERA54", "MOW10",
-    "MQC10", "MRI10", "NAB10", "NEO10", "NSE10", "PAC05", "PAC10",
-    "PAC15", "PAC20", "PAC30", "PAF10", "PAI10", "PAI20", "PAL05",
-    "PAL60", "PAL80", "PAL85", "PAL90", "PAL95", "PAP57", "PAP58",
-    "PAP59", "PAP60", "PAS51", "PAS54", "PAS55", "PAS56", "PAS58",
-    "PAU52", "PAU53", "PAU57", "PAV40", "PAZ10", "PBF10", "PBF41",
-    "PBO15", "PBT10", "PCA20", "PCB11", "PCB13", "PCN14", "PCR10",
-    "PCS10", "PCS20", "PCS30", "PDA20", "PDA30", "PDA40", "PDN10",
-    "PDR10", "PDV20", "PDV30", "PGM20", "PHA10", "PHA20", "PHA30",
-    "PHA40", "PHA72", "PIN10", "PKF10", "PMF21", "PNA70", "PNH30",
-    "PNH40", "PNH50", "PNH60", "PNM10", "PNP40", "PNR40", "POF10",
-    "POM20", "POP21", "POR25", "PPC20", "PPS10", "PQF11", "PQF20",
-    "PRB10", "PRB20", "PRC40", "PRE10", "PRF10", "PRF20", "PRH40",
-    "PRM05", "PRM40", "PRN40", "PRO10", "PRP4", "PRP4-B", "PRP6",
-    "PRR50", "PRS10", "PRT10", "PRV40", "PSB41", "PSC41", "PSE41",
-    "PSR41", "PSS41", "PSY17", "PTA10", "PTR10", "PTR11", "PVC10",
-    "PVC74", "PVF08", "PVF15", "PVF41", "PVI10", "PVI30", "PVM41",
-    "PXC10", "PXC20", "PXC30", "PXC40", "PYF10", "RAA10", "RAA20",
-    "RAC10", "RAC20", "RAH10", "RAH20", "RAH35", "RAL10", "RAM30",
-    "RAO10", "RAP10", "RAR10", "RAS10", "RAS30", "RAS40", "RAS50",
-    "RAT10", "RAT20", "RAT30", "RAT40", "RAT41", "RBA10", "RBA30",
-    "RBA40", "RBA50", "RBA60", "RCO10", "RCU10", "REA10", "REC10",
-    "REM20", "REP10", "REP11", "REP20", "REV10", "RHA30", "RHU20",
-    "RIE40", "RIE60", "RLA10", "RLA30", "RLC30", "RLD30", "RLE30",
-    "RPA05", "RPA10", "RPE30", "RPR10", "RPS10", "RPT10", "RPT15",
-    "RRE10", "RRN20", "RRZ10", "RSA10", "RSE10", "RST10", "RTA10",
-    "RTA20", "RTE10", "RTP10", "RTP20", "RVA40", "SAA10", "SAC10",
-    "SAC20", "SAE20", "SAR10", "SAT10", "SBA10", "SBC20", "SBL10",
-    "SBU10", "SCH10", "SET10", "SFM10", "SIP10", "SLP01", "SME10",
-    "SMI10", "SNI10", "SPM10", "SSO10", "STO10", "TAM30", "TMB10",
-    "TMB11", "TMB12", "TRX20", "TRX40", "TTI06", "TTI50", "VPS10",
-    "AAB80", "AAS10", "AAV10", "AAH30", "AHG10"
-    ];
+            "AAE70", "AAM10", "AAM11", "AAM12", "AAN20", "AAN30", "AAN50",
+            "AAP10", "AAY10", "ABA10", "ABA20", "ABA30", "ABA31", "ABB20",
+            "ABL10", "ABV30", "ACA10", "ACC10", "ACC20", "ACT20", "ADC10",
+            "ADC30", "ADG10", "ADI10", "ADI20", "ADI30", "ADL10", "ADN10",
+            "ADN11", "ADN12", "ADN13", "ADN14", "ADN30", "ADO10", "ADP10",
+            "ADP20", "ADR10", "ADS10", "ADT20", "ADV10", "ADV30", "AED10",
+            "AEE10", "AFM10", "AFW10", "AHA10", "AHS20", "AHU30", "AHU40",
+            "AHU50", "AIC30", "AIM10", "AMP10", "ANR10", "ANV10", "APC10",
+            "APC20", "APC34", "APZ10", "ASC10", "ASF10", "ASO10", "AST10",
+            "ATD10", "ATI11", "ATI20", "ATI30", "ATT20", "ATT30", "ATT40",
+            "CAC10", "CAH10", "CAT10", "CCA20", "CCA40", "CCH10", "CCH11",
+            "CCH70", "CCM55", "CCM60", "CCP10", "CCP15", "CCU20", "CEE20",
+            "CEH10", "CFC10", "CFP10", "CML10", "CPE10", "CPF10", "CVG10",
+            "DSA10", "EAA10", "EAV10", "ECE20", "ECE22", "ECN20", "ENV10",
+            "ERA10", "ERA20", "ERA50", "ERA51", "ERA53", "ERA54", "MOW10",
+            "MQC10", "MRI10", "NAB10", "NEO10", "NSE10", "PAC05", "PAC10",
+            "PAC15", "PAC20", "PAC30", "PAF10", "PAI10", "PAI20", "PAL05",
+            "PAL60", "PAL80", "PAL85", "PAL90", "PAL95", "PAP57", "PAP58",
+            "PAP59", "PAP60", "PAS51", "PAS54", "PAS55", "PAS56", "PAS58",
+            "PAU52", "PAU53", "PAU57", "PAV40", "PAZ10", "PBF10", "PBF41",
+            "PBO15", "PBT10", "PCA20", "PCB11", "PCB13", "PCN14", "PCR10",
+            "PCS10", "PCS20", "PCS30", "PDA20", "PDA30", "PDA40", "PDN10",
+            "PDR10", "PDV20", "PDV30", "PGM20", "PHA10", "PHA20", "PHA30",
+            "PHA40", "PHA72", "PIN10", "PKF10", "PMF21", "PNA70", "PNH30",
+            "PNH40", "PNH50", "PNH60", "PNM10", "PNP40", "PNR40", "POF10",
+            "POM20", "POP21", "POR25", "PPC20", "PPS10", "PQF11", "PQF20",
+            "PRB10", "PRB20", "PRC40", "PRE10", "PRF10", "PRF20", "PRH40",
+            "PRM05", "PRM40", "PRN40", "PRO10", "PRP4", "PRP4-B", "PRP6",
+            "PRR50", "PRS10", "PRT10", "PRV40", "PSB41", "PSC41", "PSE41",
+            "PSR41", "PSS41", "PSY17", "PTA10", "PTR10", "PTR11", "PVC10",
+            "PVC74", "PVF08", "PVF15", "PVF41", "PVI10", "PVI30", "PVM41",
+            "PXC10", "PXC20", "PXC30", "PXC40", "PYF10", "RAA10", "RAA20",
+            "RAC10", "RAC20", "RAH10", "RAH20", "RAH35", "RAL10", "RAM30",
+            "RAO10", "RAP10", "RAR10", "RAS10", "RAS30", "RAS40", "RAS50",
+            "RAT10", "RAT20", "RAT30", "RAT40", "RAT41", "RBA10", "RBA30",
+            "RBA40", "RBA50", "RBA60", "RCO10", "RCU10", "REA10", "REC10",
+            "REM20", "REP10", "REP11", "REP20", "REV10", "RHA30", "RHU20",
+            "RIE40", "RIE60", "RLA10", "RLA30", "RLC30", "RLD30", "RLE30",
+            "RPA05", "RPA10", "RPE30", "RPR10", "RPS10", "RPT10", "RPT15",
+            "RRE10", "RRN20", "RRZ10", "RSA10", "RSE10", "RST10", "RTA10",
+            "RTA20", "RTE10", "RTP10", "RTP20", "RVA40", "SAA10", "SAC10",
+            "SAC20", "SAE20", "SAR10", "SAT10", "SBA10", "SBC20", "SBL10",
+            "SBU10", "SCH10", "SET10", "SFM10", "SIP10", "SLP01", "SME10",
+            "SMI10", "SNI10", "SPM10", "SSO10", "STO10", "TAM30", "TMB10",
+            "TMB11", "TMB12", "TRX20", "TRX40", "TTI06", "TTI50", "VPS10",
+            "AAB80", "AAS10", "AAV10", "AAH30", "AHG10"
+        ]
 
         output_path = highlight_pdf_with_rondas_folios(
             input_path,
@@ -263,7 +241,6 @@ def procesar_pdf():
             download_name=f"resultado_rondas_y_folios.pdf",
             mimetype="application/pdf"
         )
-
         @response.call_on_close
         def cleanup():
             try:
@@ -271,9 +248,7 @@ def procesar_pdf():
                 os.remove(output_path)
             except OSError as e:
                 print(f"Error durante la limpieza de archivos temporales: {e}")
-
         return response
-    
     except Exception as e:
         print(f"Ocurrió un error inesperado en la ruta /procesar_pdf: {e}")
         return jsonify({"error": "Ocurrió un error interno del servidor."}), 500
